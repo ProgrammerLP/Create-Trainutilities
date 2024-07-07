29fdaab8f8805803f453814cedc6b5228e578824
ProgrammerLP <developer.programmerlp@gmx.net>
2024-07-07 15:51:14 +0200

    new setup

---
+++ common/src/main/java/net/adeptstack/ExampleMod.java
@@ -0,0 +1,9 @@
+package net.adeptstack;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "trainutilities";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
