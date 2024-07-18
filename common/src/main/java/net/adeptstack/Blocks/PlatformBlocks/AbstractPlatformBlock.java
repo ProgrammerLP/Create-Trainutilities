82631a9421dbaf40081628266487a64dbf09fb16
ProgrammerLP <developer.programmerlp@gmx.net>
2024-07-18 14:45:49 +0200

    first steps for z-axis positioning

--- common/src/main/java/net/adeptstack/Blocks/PlatformBlocks/AbstractPlatformBlock.java
+++ common/src/main/java/net/adeptstack/Blocks/PlatformBlocks/AbstractPlatformBlock.java
@@ -1,5 +1,6 @@
 package net.adeptstack.Blocks.PlatformBlocks;
 
+import net.adeptstack.Blocks.Behaviour.EBlockPlacePosition;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
@@ -12,12 +13,15 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class AbstractPlatformBlock extends Block {
 
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+    public static final EnumProperty<EBlockPlacePosition> Z_ALIGN = EnumProperty.create("z_align", EBlockPlacePosition.class);
+
     private static final VoxelShape SHAPE_SN = Block.box(0, 0, 7, 16, 16, 9);
     private static final VoxelShape SHAPE_EW = Block.box(7, 0, 0, 9, 16, 16);
 
@@ -47,11 +51,38 @@ public class AbstractPlatformBlock extends Block {
 
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+        return getDefaultPlacementState(context);
     }
 
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
         return pState.getValue(FACING) == Direction.NORTH || pState.getValue(FACING) == Direction.SOUTH ? SHAPE_SN : SHAPE_EW;
+    }
+
+    public BlockState getDefaultPlacementState(BlockPlaceContext context)  {
+        BlockState stateForPlacement = super.getStateForPlacement(context);
+        Direction direction = context.getClickedFace();
+        Direction looking = context.getHorizontalDirection();
+        Direction.Axis axis = looking.getAxis();
+        Direction.AxisDirection axisDirection = looking.getAxisDirection();
+
+        double xzPos = 0.5f;
+        if (axis == Direction.Axis.X) {
+            xzPos = context.getClickLocation().x - context.getClickedPos().getX();
+        } else if (axis == Direction.Axis.Z) {
+            xzPos = context.getClickLocation().z - context.getClickedPos().getZ();
+        }
+
+        EBlockPlacePosition zAlign = EBlockPlacePosition.CENTER;
+
+        if (direction == context.getPlayer().getDirection().getOpposite() || (axisDirection == Direction.AxisDirection.POSITIVE ? xzPos > 0.66666666D : xzPos < 8.33333333D)) {
+            zAlign = EBlockPlacePosition.POSITIVE;
+        }  else if (direction == context.getPlayer().getDirection() || (axisDirection == Direction.AxisDirection.POSITIVE ? xzPos < 0.33333333D : xzPos > 0.66666666D)) {
+            zAlign = EBlockPlacePosition.NEGATIVE;
+        }
+
+        return stateForPlacement
+                .setValue(FACING, context.getHorizontalDirection().getOpposite())
+                .setValue(Z_ALIGN, zAlign);
     }
 }
