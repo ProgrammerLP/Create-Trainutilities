ff0a3d210f9d368665b6d1bee96eff335a0c9f5d
ProgrammerLP <developer.programmerlp@gmx.net>
2024-07-18 23:12:51 +0200

    added z-positioning for platform blocks

--- common/src/main/java/net/adeptstack/Blocks/PlatformBlocks/AbstractPlatformBlock.java
+++ common/src/main/java/net/adeptstack/Blocks/PlatformBlocks/AbstractPlatformBlock.java
@@ -22,14 +22,21 @@ public class AbstractPlatformBlock extends Block {
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     public static final EnumProperty<EBlockPlacePosition> Z_ALIGN = EnumProperty.create("z_align", EBlockPlacePosition.class);
 
-    private static final VoxelShape SHAPE_SN = Block.box(0, 0, 7, 16, 16, 9);
-    private static final VoxelShape SHAPE_EW = Block.box(7, 0, 0, 9, 16, 16);
+    private static final VoxelShape SHAPE_CT_SN = Block.box(0, 0, 7, 16, 16, 9);
+    private static final VoxelShape SHAPE_CT_EW = Block.box(7, 0, 0, 9, 16, 16);
+
+    private static final VoxelShape SHAPE_S = Block.box(0, 0, 0, 16, 16, 2);
+    private static final VoxelShape SHAPE_E = Block.box(0, 0, 0, 2, 16, 16);
+
+    private static final VoxelShape SHAPE_N = Block.box(0, 0, 14, 16, 16, 16);
+    private static final VoxelShape SHAPE_W = Block.box(14, 0, 0, 16, 16, 16);
 
     public AbstractPlatformBlock(BlockBehaviour.Properties p_49795_) {
         super(p_49795_);
 
         this.registerDefaultState(this.stateDefinition.any()
                 .setValue(FACING, Direction.NORTH)
+                .setValue(Z_ALIGN, EBlockPlacePosition.CENTER)
         );
     }
 
@@ -46,7 +53,7 @@ public class AbstractPlatformBlock extends Block {
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         super.createBlockStateDefinition(pBuilder);
-        pBuilder.add(FACING);
+        pBuilder.add(FACING, Z_ALIGN);
     }
 
     @Override
@@ -56,7 +63,16 @@ public class AbstractPlatformBlock extends Block {
 
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
-        return pState.getValue(FACING) == Direction.NORTH || pState.getValue(FACING) == Direction.SOUTH ? SHAPE_SN : SHAPE_EW;
+
+        if (pState.getValue(Z_ALIGN) == EBlockPlacePosition.POSITIVE) {
+            return pState.getValue(FACING) == Direction.NORTH ? SHAPE_N : pState.getValue(FACING) == Direction.WEST ? SHAPE_W : pState.getValue(FACING) == Direction.SOUTH ? SHAPE_S : SHAPE_E;
+        }
+        else if (pState.getValue(Z_ALIGN) == EBlockPlacePosition.NEGATIVE) {
+            return pState.getValue(FACING) == Direction.NORTH ? SHAPE_S : pState.getValue(FACING) == Direction.WEST ? SHAPE_E : pState.getValue(FACING) == Direction.SOUTH ? SHAPE_N : SHAPE_W;
+        }
+        else {
+            return pState.getValue(FACING) == Direction.NORTH || pState.getValue(FACING) == Direction.SOUTH ? SHAPE_CT_SN : SHAPE_CT_EW;
+        }
     }
 
     public BlockState getDefaultPlacementState(BlockPlaceContext context)  {
@@ -75,7 +91,7 @@ public class AbstractPlatformBlock extends Block {
 
         EBlockPlacePosition zAlign = EBlockPlacePosition.CENTER;
 
-        if (direction == context.getPlayer().getDirection().getOpposite() || (axisDirection == Direction.AxisDirection.POSITIVE ? xzPos > 0.66666666D : xzPos < 8.33333333D)) {
+        if (direction == context.getPlayer().getDirection().getOpposite() || (axisDirection == Direction.AxisDirection.POSITIVE ? xzPos > 0.66666666D : xzPos < 0.33333333D)) {
             zAlign = EBlockPlacePosition.POSITIVE;
         }  else if (direction == context.getPlayer().getDirection() || (axisDirection == Direction.AxisDirection.POSITIVE ? xzPos < 0.33333333D : xzPos > 0.66666666D)) {
             zAlign = EBlockPlacePosition.NEGATIVE;
