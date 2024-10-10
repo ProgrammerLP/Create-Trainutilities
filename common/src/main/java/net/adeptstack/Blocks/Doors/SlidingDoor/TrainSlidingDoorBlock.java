2f4d665b83e4329afd64ef32b8347946a42ebcd6
ProgrammerLP <developer.programmerlp@gmx.net>
2024-10-10 22:28:36 +0200

    added item descriptions for all TrainSlidingDoorBlocks

--- common/src/main/java/net/adeptstack/Blocks/Doors/SlidingDoor/TrainSlidingDoorBlock.java
+++ common/src/main/java/net/adeptstack/Blocks/Doors/SlidingDoor/TrainSlidingDoorBlock.java
@@ -1,12 +1,18 @@
 package net.adeptstack.Blocks.Doors.SlidingDoor;
 
 import com.simibubi.create.content.decoration.slidingDoor.SlidingDoorBlock;
+import com.simibubi.create.foundation.item.ItemDescription;
+import com.simibubi.create.foundation.item.KineticStats;
+import com.simibubi.create.foundation.item.TooltipHelper;
+import com.simibubi.create.foundation.item.TooltipModifier;
 import net.adeptstack.Core.Client.ClientWrapper;
 import net.adeptstack.registry.ModBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -15,6 +21,9 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockSetType;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
+
+import static net.adeptstack.Main.REGISTRATE;
 
 public class TrainSlidingDoorBlock extends SlidingDoorBlock {
 
--- common/src/main/java/net/adeptstack/Main.java
+++ common/src/main/java/net/adeptstack/Main.java
@@ -1,12 +1,21 @@
 package net.adeptstack;
 
 import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.simibubi.create.foundation.item.ItemDescription;
+import com.simibubi.create.foundation.item.KineticStats;
+import com.simibubi.create.foundation.item.TooltipHelper;
+import com.simibubi.create.foundation.item.TooltipModifier;
 import com.tterrag.registrate.providers.ProviderType;
+import net.adeptstack.Blocks.Doors.SlidingDoor.TrainSlidingDoorBlock;
 import net.adeptstack.Core.Network.ModNetwork;
 import net.adeptstack.registry.*;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DataProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.Nullable;
 
 public final class Main {
     public static final String MOD_ID = "trainutilities";
@@ -30,5 +39,23 @@ public final class Main {
 
     public static ResourceLocation asResource(String path) {
         return new ResourceLocation(MOD_ID, path);
+    }
+
+    @Nullable
+    public static KineticStats create(Item item) {
+        if (item instanceof BlockItem blockItem) {
+            Block block = blockItem.getBlock();
+            if (block instanceof TrainSlidingDoorBlock) {
+                return new KineticStats(block);
+            }
+            // ...
+        }
+        return null;
+    }
+
+    static {
+        REGISTRATE.setTooltipModifierFactory(item -> {
+            return new ItemDescription.Modifier(item, TooltipHelper.Palette.STANDARD_CREATE).andThen(TooltipModifier.mapNull(create(item)));
+        });
     }
 }
