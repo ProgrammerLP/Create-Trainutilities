8d1e4480040703dfa0e1e992efedaf9eaf1ad341
ProgrammerLP <developer.programmerlp@gmx.net>
2025-07-02 16:07:22 +0200

    beginning of 1.21.1 port

---
+++ neoforge/src/main/java/net/adeptstack/neoforge/ExampleModNeoForge.java
@@ -0,0 +1,13 @@
+package net.adeptstack.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import net.adeptstack.ExampleMod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleModNeoForge {
+    public ExampleModNeoForge() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
