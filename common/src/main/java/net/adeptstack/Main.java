711f01c35c6c75cd93208fca9bdcdbf1dcd29537
ProgrammerLP <info@adeptstack.net>
2026-08-01 14:18:52 +0200

    add "marked for removal" hints in tooltips and on startup

--- common/src/main/java/net/adeptstack/Main.java
+++ common/src/main/java/net/adeptstack/Main.java
@@ -5,12 +5,15 @@ import com.simibubi.create.foundation.item.ItemDescription;
 import com.simibubi.create.foundation.item.KineticStats;
 import com.simibubi.create.foundation.item.TooltipHelper;
 import com.simibubi.create.foundation.item.TooltipModifier;
+import dev.architectury.event.events.common.PlayerEvent;
 import dev.architectury.platform.Platform;
 import net.adeptstack.blocks.doors.slidingDoor.TrainSlidingDoorBlock;
 import net.adeptstack.network.ModNetwork;
 import net.adeptstack.registry.*;
 import net.createmod.catnip.lang.FontHelper;
 import net.fabricmc.api.EnvType;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
@@ -22,7 +25,6 @@ public final class Main {
     public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(MOD_ID);
 
     public static void init() {
-        // Write common init code here.
         ModBlockEntities.register();
         ModTabs.CREATIVE_MODE_TABS.register();
         ModBlocks.register();
@@ -33,6 +35,17 @@ public final class Main {
             ModPartialModels.init();
         }
         ModTags.register();
+
+        PlayerEvent.PLAYER_JOIN.register(player -> {
+            if (!player.level().isClientSide()) {
+                Component message = Component.literal("""
+                                ATTENTION (Create Train Utilities):\s
+                                Create Train Utilities will be renamed Create Train Doors in version 4.
+                                All platform blocks and noise isolation walls will be removed in v4. Use our new mod, “Create TrackSta,” to continue using these blocks!""")
+                        .withStyle(ChatFormatting.RED, ChatFormatting.BOLD);
+                player.sendSystemMessage(message);
+            }
+        });
     }
 
     public static ResourceLocation asResource(String path) {
