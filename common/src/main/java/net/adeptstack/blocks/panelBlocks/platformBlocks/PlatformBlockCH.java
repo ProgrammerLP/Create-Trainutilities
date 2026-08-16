a73cde760924280a483df5f44131a4ff1215a499
ProgrammerLP <info@adeptstack.net>
2026-08-16 21:51:22 +0200

    changing in adventure mode isnt possible anymore

--- common/src/main/java/net/adeptstack/blocks/panelBlocks/platformBlocks/PlatformBlockCH.java
+++ common/src/main/java/net/adeptstack/blocks/panelBlocks/platformBlocks/PlatformBlockCH.java
@@ -2,10 +2,12 @@ package net.adeptstack.blocks.panelBlocks.platformBlocks;
 
 import net.adeptstack.blocks.panelBlocks.PanelBlockBase;
 import net.adeptstack.client.ClientWrapper;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.GameType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -32,8 +34,19 @@ public class PlatformBlockCH extends PanelBlockBase {
 
     @Override
     public InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hit) {
-        if (level.isClientSide) {
-            ClientWrapper.openPlatformBlockCHScreen(pos, state);
+        if (!level.isClientSide() && player instanceof ServerPlayer serverPlayer) {
+            if (serverPlayer.gameMode.getGameModeForPlayer() == GameType.ADVENTURE) {
+                return InteractionResult.PASS;
+            }
+        }
+
+        if (level.isClientSide()) {
+            if (Minecraft.getInstance().gameMode != null && Minecraft.getInstance().gameMode.getPlayerMode() != GameType.ADVENTURE) {
+                ClientWrapper.openPlatformBlockCHScreen(pos, state);
+                return InteractionResult.SUCCESS;
+            } else {
+                return InteractionResult.PASS;
+            }
         }
         return InteractionResult.SUCCESS;
     }
